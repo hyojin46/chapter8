9cd3a9e36f00fc86edc0dad46bf9f750ac45766f
hyojin park <candy020406@naver.com>
2022-11-23 20:50:44 +0900

    ㄴ

--- chapter8/7.Designer.cs
+++ chapter8/7.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 323);
+            this.ClientSize = new System.Drawing.Size(315, 227);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
