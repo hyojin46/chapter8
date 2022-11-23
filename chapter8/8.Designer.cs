c06c0e1c8b8a9afc034a68f2a678457b9c9c25e5
hyojin park <candy020406@naver.com>
2022-11-23 21:05:51 +0900

    s

--- chapter8/8.Designer.cs
+++ chapter8/8.Designer.cs
@@ -56,6 +56,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = ">>";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -65,6 +66,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "<<";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
