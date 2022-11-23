17fdc99fb8152d5caa478feae6cd73018981c36c
hyojin park <candy020406@naver.com>
2022-11-23 21:06:07 +0900

    s

--- chapter8/8.cs
+++ chapter8/8.cs
@@ -19,7 +19,11 @@ namespace chapter8
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (listBox1.SelectedItem != null)
+            {
+                comboBox1.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
