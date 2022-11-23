7d83c201d6d2577958ee57f8b62d1dbbcad74ec8
hyojin park <candy020406@naver.com>
2022-11-23 20:25:12 +0900

    s

--- chapter8/3.cs
+++ chapter8/3.cs
@@ -24,7 +24,7 @@ namespace chapter8
         }
         private void radioButton2_CheckedChanged(object sender, EventArgs e) //라디오 버튼 이벤트 처리기
         {
-            label1.Text = radioButton2.Text;
+            label1.Text = radioButton2.Text; //라디오 버튼의 이름을 라벨에 출력
         }
         private void radioButton3_CheckedChanged(object sender, EventArgs e) //라디오 버튼 이벤트 처리기
         {
