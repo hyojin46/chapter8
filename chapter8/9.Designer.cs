a1bd56e093a7a2f3b0965b4a2c8f61d470c6abfb
hyojin park <candy020406@naver.com>
2022-11-23 21:11:03 +0900

    ㄴ

--- chapter8/9.Designer.cs
+++ chapter8/9.Designer.cs
@@ -70,6 +70,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "선택";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // _9
             // 
--- chapter8/9.cs
+++ chapter8/9.cs
@@ -16,5 +16,10 @@ namespace chapter8
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
