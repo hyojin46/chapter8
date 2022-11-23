80999531257dda34e113174a53101f35d95d903d
hyojin park <candy020406@naver.com>
2022-11-23 20:49:51 +0900

    ㄴ

---
+++ chapter8/7.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace chapter8
+{
+    partial class _7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // _7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 323);
+            this.Name = "_7";
+            this.Text = "_7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
