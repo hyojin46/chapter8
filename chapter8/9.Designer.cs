95be9df8165ee219605e08c280ecfe960e50a40c
hyojin park <candy020406@naver.com>
2022-11-23 21:09:46 +0900

    ㄴ

--- chapter8/9.Designer.cs
+++ chapter8/9.Designer.cs
@@ -38,9 +38,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(109, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "취미를 선택하시오.";
             // 
             // checkedListBox1
             // 
